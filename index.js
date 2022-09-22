a61a2dd004c8bff7ebfcd58c5fd3ebbb08c25b0f
Kimber Herron <kh582013@gmail.com>
2022-09-22 19:46:59 -0400

    commit tree d923741284f916006f3b419d361161160171a35a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = ([Antonia, Nuru, Amari, Mo]) => [Antonia, Nuru]
+const returnLastTwoDrivers = ([Antonia, Nuru, Amari, Mo]) => [Amari, Mo]
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier() {
+    return function(fare) {
+        return fare ** 2;
+    }
+}
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = function(fare) {return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = (fare) => 
+createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers (arrayOfDrivers, returnFirstTwoDrivers) {
+    {return returnFirstTwoDrivers()
+    }
+    arrayOfDrivers(selectDifferentDrivers)
+}
